fe6676059e747c555ceba9a1f5c0e82f48c093ba
flapjackstan <elm.s.camargo@gmail.com>
2023-01-31 08:31:00 -0800

    got view to process request and added print statements as well as included custom html to access context

--- taza/linkinbio/views.py
+++ taza/linkinbio/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from .models import listItems
+from django.template import loader
 
-# Create your views here.
+# https://docs.djangoproject.com/en/4.1/ref/request-response/#django.http.HttpResponse
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the linkinbio index.")
+
+    print("REQUEST START")
+    print(request.headers)
+    print(request.META)
+
+    items = listItems.objects.all()
+
+    template = loader.get_template('linkinbio/index.html')
+
+    context = {
+        'items_list': items
+    }
+
+    response = HttpResponse(template.render(context, request))
+
+    # print("RESPONSE START")
+    # print(response.headers)
+    return response
